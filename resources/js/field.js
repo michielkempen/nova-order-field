112970ee47e80598ee6c19f900caddeb77d08ba5
Michael <michael.klopf@suenert.de>
2024-01-23 17:36:33 +0100

    Follow the upgrade guide

--- resources/js/field.js
+++ resources/js/field.js
@@ -1,3 +1,6 @@
 Nova.booting((Vue, router) => {
-    Vue.component('index-order-field', require('./components/IndexField'));
-})
+  Vue.component(
+    "index-order-field",
+    require("./components/IndexField").default
+  );
+});
